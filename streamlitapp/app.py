b5ecb2ab9d4c32374113d11a7c292f762d986956
CARLOS EDUARDO NEVES <carlos.vneves@gmail.com>
2024-05-07 18:56:52 -0300

    documentação e esqueleto da aplicação

---
+++ streamlitapp/app.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+st.write("# Welcome to my app!")
+
+from google.oauth2 import service_account
+from google.cloud import bigquery
+
+# Create API client.
+credentials = service_account.Credentials.from_service_account_info(
+    st.secrets["gcp_service_account"]
+)
+client = bigquery.Client(credentials=credentials)
+
+# Perform query.
+# Uses st.cache_data to only rerun when the query changes or after 10 min.
+@st.cache_data(ttl=600)
+def run_query(query):
+    query_job = client.query(query)
+    rows_raw = query_job.result()
+    # Convert to list of dicts. Required for st.cache_data to hash the return value.
+    rows = [dict(row) for row in rows_raw]
+    return rows
+
+rows = run_query("SELECT * FROM `postechfiaptc03may2024.microdados_covid19.view_dados_gerais` LIMIT 10")
+
+# Print results.
+st.write("Some:")
+for row in rows:
+    st.write(row)
+
+
